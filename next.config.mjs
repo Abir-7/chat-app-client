6879206d9259b0644bc7a58fc2c2231b8954b8f1
Md. Tazwarul Islam Abir <md.tazwarul.islam.07@gmail.com>
2025-01-26 22:43:10 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "chat-app-server-fpdu.onrender"],
+    domains: ["localhost", "chatapp-client-beta.vercel.app"],
   },
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "chatapp-client-beta.vercel.app", // Allow images from your app's domain
+      pathname: "/images/**", // Adjust this path based on your image storage structure
+    },
+  ],
 };
 
 export default nextConfig;
